cb05521c68a0cf4fff575dff1599dcb9615c8afe
Samuel Cesla <d23755@oa-opava.cz>
2026-06-10 09:23:05 +0200

    ..

--- Artist_List/settings.py
+++ Artist_List/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l&*-r-ozzz&=)m80)eu&$dxphc%ygf6@s4iv+&tk_(%k4yeqwb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['samuelcesla.pythonanywhere.com',]
+ALLOWED_HOSTS = ['samuelcesla.pythonanywhere.com', 'http://127.0.0.1:8000/']
 
 
 # Application definition
