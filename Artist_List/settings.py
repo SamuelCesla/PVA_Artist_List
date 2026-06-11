b095e8e4e8d8a53f743d694e085f345726000269
Samuel Cesla <d23755@oa-opava.cz>
2026-06-11 10:05:57 +0200

    css

--- Artist_List/settings.py
+++ Artist_List/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l&*-r-ozzz&=)m80)eu&$dxphc%ygf6@s4iv+&tk_(%k4yeqwb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['samuelcesla.pythonanywhere.com', 'http://127.0.0.1:8000/']
+ALLOWED_HOSTS = ['samuelcesla.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -117,6 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'Interpreti' / 'static',
+]
 
 MEDIA_URL = '/media/'
 
